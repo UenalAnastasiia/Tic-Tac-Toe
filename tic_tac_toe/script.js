99a964acf03b7b703c0d988343621010304adf0a
UenalAnastasiia <velyka.nastya@icloud.com>
2022-05-24 15:30:16 +0200

    first commit

---
+++ tic_tac_toe/script.js
@@ -0,0 +1,14 @@
+let fields = [];
+
+let currentShape = 'cross';
+
+function fillShape(id) {
+    if(currentShape == 'cross') {
+        currentShape = 'circle';
+    } else {
+        currentShape = 'cross';
+    }
+
+    fields[id] = currentShape;
+    console.log(fields);
+}
