6303d0a4c540f8a34c04c87d7b95b6f383e9beea
UenalAnastasiia <velyka.nastya@icloud.com>
2022-05-25 16:38:08 +0200

    Update

--- tic_tac_toe/script.js
+++ tic_tac_toe/script.js
@@ -2,7 +2,6 @@ let fields = [];
 let gameOver = false;
 let currentShape = 'cross';
 
-
 function fillShape(id) {
     if (!fields[id] && !gameOver) {    // wenn "nicht gameover" (!gameOver)
         if (currentShape == 'cross') {
@@ -17,23 +16,6 @@ function fillShape(id) {
 
         fields[id] = currentShape;
         draw();
-    }
-}
-
-
-function restart() {
-    gameOver = false;
-    fields = [];
-    document.getElementById('game-over').classList.add('d-none');
-    document.getElementById('restart-btn').classList.add('d-none');
-
-    for (let i = 1; i < 8; i++) {
-        document.getElementById('line-' + i).classList.add('d-none');
-    }
-
-    for (let i = 0; i < 9; i++) {
-        document.getElementById('circle-' + i).classList.add('d-none');
-        document.getElementById('cross-' + i).classList.add('d-none');
     }
 }
 
@@ -100,6 +82,23 @@ function checkForWin() {
         setTimeout(function() {
             document.getElementById('game-over').classList.remove('d-none');
             document.getElementById('restart-btn').classList.remove('d-none');
-        }, 2000);
+        }, 1000);
+    }
+}
+
+
+function restart() {
+    gameOver = false;
+    fields = [];
+    document.getElementById('game-over').classList.add('d-none');
+    document.getElementById('restart-btn').classList.add('d-none');
+
+    for (let i = 1; i < 8; i++) {
+        document.getElementById('line-' + i).style.transform = 'scaleX(0)';
+    }
+
+    for (let i = 0; i < 9; i++) {
+        document.getElementById('circle-' + i).classList.add('d-none');
+        document.getElementById('cross-' + i).classList.add('d-none');
     }
 }
